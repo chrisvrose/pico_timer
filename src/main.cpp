f9b700e4e3ada91878444c8853ae8651f9c649bf
Atreya Bain <atreyabain@gmail.com>
2026-02-08 20:09:41 +0000

    [ref] Replace file

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,54 @@
-#include <stdio.h>
-#include "hardware/i2c.h"
+#include <pico/assert.h>
+#include <pico/stdio.h>
+#include <pico/stdio_usb.h>
+#include <pico/time.h>
+#include <pico/types.h>
 #include "hardware/rtc.h"
-#include "pico-ssd1306/ssd1306.h"
-#include "pico-ssd1306/textRenderer/TextRenderer.h"
-#include "pico.h"
-#include "pico/stdlib.h"
-#include "pico/util/datetime.h"
-#include "tusb.h"
+#include "context.hh"
+extern "C"{
+#include "pico/status_led.h"
+}
 
-int main() {
-    const uint led_pin = 25;
+bool led_state = true;
 
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
+#define WAIT_FOR_USB
 
-    // Initialize chosen serial port
+const datetime_t update_alarm = {
+    .year=-1,
+    .month=-1,
+    .day=-1,
+    .dotw=-1,
+    .hour=-1,
+    .min=-1,
+    .sec=00,
+};
+
+void update_alarm_callback(){
+    status_led_set_state(led_state);
+    led_state = !led_state;
+}
+
+int main(){
     stdio_init_all();
     rtc_init();
+    bool ret = status_led_init();
+    hard_assert(ret);
+    rtc_enable_alarm();
 
-    // printf("waiting for usb host");
-    // while (!tud_cdc_connected()) {
-    //     printf(".");
-    //     sleep_ms(500);
-    // }
-    // printf("\nusb host detected!\n");
-
-    i2c_init(i2c_default, 100 * 1000);
-
-    // i2c_init(i2c0, 1000000); //Use i2c port with baud rate of 1Mhz
-    // //Set pins for I2C operation
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-
-    // //Create a new display object
-    pico_ssd1306::SSD1306 display =
-        pico_ssd1306::SSD1306(i2c_default, 0x3C, pico_ssd1306::Size::W128xH64);
-    datetime_t current_datetime = {.year = 2020,
-                                   .month = 06,
-                                   .day = 05,
-                                   .dotw = 5,  // 0 is Sunday, so 5 is Friday
-                                   .hour = 15,
-                                   .min = 45,
-                                   .sec = 00};
-    rtc_set_datetime(&current_datetime);
-    // create a vertical line on x: 64 y:0-63
-    for (int y = 0; y < 64; y++) {
-        display.setPixel(64, y);
-    }
-    char a[6] = "Hello";
-    pico_ssd1306::drawText(&display, font_12x16, a, 0, 0);
-    display.sendBuffer();
-
-    char s[100] = {0};
-    sleep_ms(2000);
-    // Loop forever
-    while (true) {
-        display.clear();
-        bool stat = rtc_get_datetime(&current_datetime);
-        if(stat)
-        sprintf(s, "%02d:%02d:%02d", current_datetime.hour,
-                current_datetime.min, current_datetime.sec);
-        else
-        sprintf(s,"E:RTC not running");
-        pico_ssd1306::drawText(&display, font_12x16, s, 20, 10);
-        display.sendBuffer();
-        // Blink LED
-        printf("Tick!\r\n");
+    #ifdef WAIT_FOR_USB
+    while(!stdio_usb_connected()){
         sleep_ms(1000);
     }
+    #endif
+
+    datetime_t date_mut = {};
+
+    rtc_set_alarm(&update_alarm, update_alarm_callback);
+
+    while (true) {
+        app_context_instance.dispatch();
+        sleep_ms(100);
+    }
+
+    return 0;
 }
